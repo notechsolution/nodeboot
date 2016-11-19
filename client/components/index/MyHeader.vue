05f2cdefa87e923fab5e1977d381bc8cba8c1638
notechsolution <notech@126.com>
2016-11-19 09:47:46 +0800

    logo-notech-nodeboot and fork-me-github logo, still need refine the css

--- client/components/index/MyHeader.vue
+++ client/components/index/MyHeader.vue
@@ -2,7 +2,10 @@
   <div id="header">
     <navbar placement="top" type="default">
       <!-- Brand as slot -->
-      <a slot="brand" href="/" title="Home" class="navbar-brand">VueStrap</a>
+      <a slot="brand" href="/" title="Home" class="navbar-brand nodeboot-logo">
+        <img src="../../assets/image/logo-notech-nodeboot.png" class="nodeboot-logo">
+      </a>
+
       <!-- You can use dropdown component -->
       <dropdown text="Dropdown">
         <li><a href="link">Option</a></li>
@@ -13,12 +16,19 @@
         <a href="#" @click="openLoginModal">Login</a>
       </li>
 
-      <li slot="right">
-        <a href="https://github.com/yuche/vue-strap" target="_blank">Fork...</a>
+      <li slot="right" class='fork-me-at-github'>
+        <a href="https://github.com/notechsolution/nodeboot">
+          <img style="position: absolute; top: 0; right: 0; border: 0;" src="../../assets/image/fork-me-github.png" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png">
+        </a>
       </li>
 
 
     </navbar>
+    <!--<div class='fork-me-at-github'>-->
+      <!--<a href="https://github.com/notechsolution/nodeboot">-->
+        <!--<img style="position: absolute; top: 0; right: 0; border: 0;" src="../../assets/image/fork-me-github.png" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png">-->
+      <!--</a>-->
+    <!--</div>-->
     <Login v-bind:active.sync="activeModel"></Login>
   </div>
 </template>
@@ -47,5 +57,19 @@ export default {
 </script>
 
 <style>
+.nodeboot-logo {
+     padding: 1px 5px !important;
+}
 
+.nodeboot-logo img {
+ width: 120px;
+ height: 50px;
+}
+
+.fork-me-at-github {
+ }
+
+ .fork-me-at-github img{
+  z-index: 2000
+ }
 </style>
