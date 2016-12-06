05a9d36c551dd0329e0f08e9aa29ae5b6b2d78f0
notechsolution <notech@126.com>
2016-12-07 07:19:48 +0800

    refined the user profile with data

--- client/components/user/UserProfile.vue
+++ client/components/user/UserProfile.vue
@@ -4,24 +4,12 @@
 
     <div class="panel panel-info">
       <div class="panel-heading">
-        <h3 class="panel-title">Sheena Shrestha</h3>
+        <h3 class="panel-title">{{user.username}}</h3>
       </div>
       <div class="panel-body">
         <div class="row">
           <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="http://babyinfoforyou.com/wp-content/uploads/2014/10/avatar-300x300.png" class="img-circle img-responsive"> </div>
 
-          <!--<div class="col-xs-10 col-sm-10 hidden-md hidden-lg"> <br>
-            <dl>
-              <dt>DEPARTMENT:</dt>
-              <dd>Administrator</dd>
-              <dt>HIRE DATE</dt>
-              <dd>11/12/2013</dd>
-              <dt>DATE OF BIRTH</dt>
-                 <dd>11/12/2013</dd>
-              <dt>GENDER</dt>
-              <dd>Male</dd>
-            </dl>
-          </div>-->
           <div class=" col-md-9 col-lg-9 ">
             <table class="table table-user-information">
               <tbody>
@@ -30,32 +18,21 @@
                 <td>Programming</td>
               </tr>
               <tr>
-                <td>Hire date:</td>
-                <td>06/23/2013</td>
-              </tr>
-              <tr>
-                <td>Date of Birth</td>
-                <td>01/24/1988</td>
-              </tr>
-
-              <tr>
-              <tr>
-                <td>Gender</td>
-                <td>Female</td>
-              </tr>
-              <tr>
-                <td>Home Address</td>
-                <td>Kathmandu,Nepal</td>
+                <td>User Name</td>
+                <td>{{user.username}}</td>
               </tr>
               <tr>
                 <td>Email</td>
-                <td><a href="mailto:info@support.com">info@support.com</a></td>
+                <td><a href="{{user.email}}">{{user.email}}</a></td>
               </tr>
               <tr>
-              <td>Phone Number</td>
-              <td>123-4567-890(Landline)<br><br>555-4567-890(Mobile)
+              <td>Roles</td>
+              <td>{{user.roles}}
               </td>
-
+              </tr>
+              <tr>
+                <td>Created</td>
+                <td>{{user.created}}</td>
               </tr>
 
               </tbody>
@@ -63,13 +40,6 @@
           </div>
         </div>
       </div>
-      <div class="panel-footer">
-      <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
-      <span class="pull-right">
-                            <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
-                            <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
-                        </span>
-    </div>
 
     </div>
   </div>
@@ -106,7 +76,8 @@
     border-top: 0;
 }
 .toppad
-{margin-top:20px;
+{margin-top:100px;
+background-color: #f6f6f6;
 }
 
 </style>
@@ -114,7 +85,7 @@
     export default{
         data(){
             return{
-                msg:'hello vue'
+                user: window.user || {}
             }
         },
         components:{
