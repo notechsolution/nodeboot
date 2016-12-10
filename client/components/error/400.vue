978e9c8c7fba1a86086dc766ab7387565860be5c
notechsolution <notech@126.com>
2016-12-10 16:15:25 +0800

    404 error page UI defined

--- client/components/error/400.vue
+++ client/components/error/400.vue
@@ -1,10 +1,24 @@
 <template>
-  <div id="app" class="container" style="margin-top: 10%;">
-    <h1>404 Error</h1>
-    <pre>
-  {{error}}
-</pre>
+  <div class="container">
+    <div class="row hero-unit">
+      <div class="col-md-2">
+        <img src="../../assets/image/emotiguy.png" class="emotion">
+      </div>
+      <div class="col-md-10">
+        <div class=" center">
+
+          <h1>Page Not Found <small><font face="Tahoma" color="red">Error 404</font></small></h1>
+          <br />
+          <p>The page you requested could not be found, either contact your webmaster or try again. Use your browsers <b>Back</b> button to navigate to the page you have prevously come from</p>
+          <p><b>Or you could just press this neat little button:</b></p>
+          <a href="/" class="btn btn-large btn-info"><i class="glyphicon glyphicon-home"></i> &nbsp;&nbsp;Take Me Home</a>
+        </div>
+        <br />
+        <!-- By ConnerT HTML & CSS Enthusiast -->
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -23,7 +37,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  color: #42b983;
-}
+ .center {text-align: center; margin-left: auto; margin-right: auto; margin-bottom: auto; margin-top: auto;}
+ .hero-unit {
+ margin-top:10%;
+   padding: 60px;
+  margin-bottom: 30px;
+  font-size: 18px;
+  font-weight: 200;
+  line-height: 30px;
+  color: inherit;
+  background-color: #eeeeee;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  border-radius: 6px;
+ }
+
+ .hero-unit h1 {
+   margin-bottom: 0;
+  font-size: 60px;
+  line-height: 1;
+  color: inherit;
+  letter-spacing: -1px;
+  }
+  .emotion {
+    margin-left: -200px;
+    margin-top: -100px;
+    margin-right: 30px;
+  }
 </style>
